af4d5b611a149d9022aff2bc36a15b265ecbb3b5
Emerson Lopes <emersonlbr@icloud.com>
2018-01-17 21:10:58 -0200

    pomodoro style

--- pomodoro/app.js
+++ pomodoro/app.js
@@ -1,1 +1,12 @@
 console.log('Hello friend!');
+
+const minus = document.querySelector('.minus-l'),
+      time  = document.querySelector('.time');
+
+const timeS = 25;
+
+minus.onclick = () => {
+  time.innerHTML = timeS - 1;
+};
+
+document.getElementById('.hello')
