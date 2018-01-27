eb75c31f5030df11511f973335596b01a05998bf
Emerson Lopes <emersonlbr@icloud.com>
2018-01-27 20:22:04 -0200

    tic tac toe style sample

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+console.log('hello world');
+
+const enem2017 = {
+  natureza: 528,
+  linguagens: 625,
+  humanas: 640,
+  matematica: 682,
+  redação: 780,
+};
+
+// these are my grades, possibly is the year in which I will get into college.
+// 2017 was a very hard year, but I beleeve I have grown as a human, I learned a lot of new things.
+// 2018 is going to be the year I will face more challenges in my life, a part of my dream that is going to be true.
+
