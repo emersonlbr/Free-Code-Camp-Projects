8b467b36bf4d3acf250aa715ed366540d0a299bd
Emerson Lopes <emersonlbr@icloud.com>
2018-01-28 21:48:26 -0200

    logic

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -11,4 +11,3 @@ const enem2017 = {
 // these are my grades, possibly is the year in which I will get into college.
 // 2017 was a very hard year, but I beleeve I have grown as a human, I learned a lot of new things.
 // 2018 is going to be the year I will face more challenges in my life, a part of my dream that is going to be true.
-
