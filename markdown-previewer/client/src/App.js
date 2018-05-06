9923478bcc7b22e02b1af7c9e7e8c4ed8bdd306a
lupuselit <lupuselit@outlook.com>
2018-05-06 16:02:32 -0300

    add marked and sass

--- markdown-previewer/client/src/App.js
+++ markdown-previewer/client/src/App.js
@@ -1,32 +1,47 @@
 import React, { Component } from 'react';
-import './App.css';
+import marked from 'marked';
+
+import './App.scss';
 
 class App extends Component {
-  state = {
-    text: 'write your text here'
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '# some really interesting text on here'
+
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  onChange() {
-    
+  handleChange(event) {
+    this.setState({ text: event.target.value });
   }
- 
+
   render() {
-   
     return (
       <div className="App">
-        <textarea  className='TextArea' type="text" />
-        <Result text={this.state.text}/>
+        <textarea
+          className="TextArea"
+          type="text"
+          value={this.state.text}
+          onChange={this.handleChange}
+        />
+
+        <Result text={this.state.text} />
       </div>
     );
   }
 }
 
-const Result = props => {
+const Result = (props) => {
+  // const { text } = props;
+  const markedown = marked(props.text);
   return (
-    <div>
-      <textarea className='TextArea' value={props.text}/>
-    </div>
-  )
-}
+    <div
+      className="Result"
+      dangerouslySetInnerHTML={{__html: markedown }}
+    />
+  );
+};
 
 export default App;
